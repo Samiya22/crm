9c6fa74697bfc40c02ece1e111324886e86cc0fa
samiya22 <samiya.sultanova21@gmail.com>
2022-04-12 15:04:19 +0500

    Abstract modellar

--- leads/admin.py
+++ leads/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Lead, User
+from .models import *
 
 admin.site.register(Lead)
 admin.site.register(User)
+admin.site.register(Agent)
--- leads/models.py
+++ leads/models.py
@@ -15,7 +15,14 @@ class Lead(models.Model):
     agent = models.OneToOneField("Agent", on_delete=models.CASCADE)
 
 
+    def __str__(self):
+        return self.familyasi
+
+
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+
+    def __str__(self):
+        return self.user
