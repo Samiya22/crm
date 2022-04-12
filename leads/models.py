95f1d0738194558c14b640d9d7e138afab77cc1c
samiya22 <samiya.sultanova21@gmail.com>
2022-04-12 11:24:15 +0500

    lead class create

---
+++ leads/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+
+class User(AbstractUser):
+    pass
+
+
+
+class Lead(models.Model):
+    ismi = models.CharField(max_length=20)
+    familyasi = models.CharField(max_length=20)
+    yoshi = models.IntegerField(default=0)
+
