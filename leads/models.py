b218afbac4ffbe35ff2a826bc5ea9ee9fc6f4f44
samiya22 <samiya.sultanova21@gmail.com>
2022-04-20 11:42:43 +0500

    yangilanish

--- leads/models.py
+++ leads/models.py
@@ -1,5 +1,5 @@
-from xxlimited import Str
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
@@ -26,7 +26,15 @@ class Lead(models.Model):
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profil = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
 
     def __str__(self):
         return str(self.user)
+
+def post_user_yaratish(sender, instance, created, **kwargs):
+
+    if created:
+        UserProfile.objects.create(user=instance)
+
+post_save.connect(post_user_yaratish, sender=User)
